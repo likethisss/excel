b8056f331116d8fa14e36db80d19dc719a388232
zhoon <zhoon>
2026-04-17 10:52:17 +0800

    fix: 兼容非标准 numFmtId (如41)，移除164限制

--- lib/src/parser/parse.dart
+++ lib/src/parser/parse.dart
@@ -297,8 +297,9 @@ class Parser {
           final numFmtId = int.parse(node.getAttribute('numFmtId')!);
           final formatCode = node.getAttribute('formatCode')!;
           if (numFmtId < 164) {
-            throw Exception(
-                'custom numFmtId starts at 164 but found a value of $numFmtId');
+            return;
+            // throw Exception(
+            //     'custom numFmtId starts at 164 but found a value of $numFmtId');
           }
 
           _excel._numFormats
